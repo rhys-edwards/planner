1b9b6d1c2a001d9075e00da3dffa2e490124e89c
Rhys Edwards <rhys.edwards91@gmail.com>
2016-04-01 20:59:38 +0100

    This weekend almost displays correctly

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,23 @@ var router = express.Router();
 var Entry = require('../models/entry.model');
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
+var moment = require('moment-timezone');
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //  res.render('index', { title: 'Express' });
 // });
 
 router.get('/', function(req, res, next){
-  Entry.find({}).sort('-date').exec(function(err, entries) {
-    res.render('index', {
-      "entries": entries
-    });
-  });
+  weekendPlans(function(err, theWeekend) {
+      if (err) throw err
+      console.log(theWeekend);
+      res.render('index', {"theWeekend" : theWeekend});
+  })
+  // Entry.find({}).sort('-date').exec(function(err, entries) {
+  //   res.render('index', {
+  //     "entries": entries
+  //   });
+  // });
 });
 
 router.post('/post', function(req, res, next) {
@@ -22,9 +28,13 @@ router.post('/post', function(req, res, next) {
   var anytime = req.body.anytime;
   var date = req.body.date;
 
-  var dateObj = new Date(date);
+  // CONVERT DATE TO LONDON TIME (THIS WONT LAST)
+  var london = moment.tz(date, "Europe/London");
+
+  // CONVERT DATE TO ISODATE FOR MONGO
+  var dateObj = new Date(london);
   var isoDate = dateObj.toISOString()
-  console.log(isoDate + ' ' + 'step 1')
+
   //FIND WHICH DATE WAS SELECTED BY USER AND ASSIGN THAT TO selectedDate
   if (typeof anytime != 'undefined' ){
     var selectedDate = 'anytime';
@@ -49,41 +59,65 @@ router.post('/post', function(req, res, next) {
   })
 
   //RENDER THE HOMEPAGE TO CLEAR THE FORM
-  Entry.find({}).sort('-date').exec(function(err, entries) {
-    res.render('index', {
-      "entries": entries
-    });
+  weekendPlans(function(err, theWeekend) {
+      if (err) throw err
+      console.log(theWeekend);
+      res.render('index', {"theWeekend" : theWeekend});
   });
   return false;
 });
 
-// THIS WEEKEND
-Entry.aggregate(
-    [
-        { "$redact": {
-            "$cond": {
-                "if": {
-                    "$or": [
-                        { "$eq": [ { "$dayOfWeek": "$selectedDate" }, 1 ] },
-                        { "$eq": [ { "$dayOfWeek": "$selectedDate" }, 7 ] }
-                    ]
-                },
-                "then": "$$KEEP",
-                "else": "$$PRUNE"
-            }
-        }}
-    ],
-    function(err,results) {
-      if (err) throw err;
-      console.log(results);
+// DETERMINE THE DATES FOR THIS WEEKEND
+var yesterday = new Date();
+yesterday.setDate(yesterday.getDate());
+var dowOffset = (yesterday.getUTCDay() - 1) % 7;
+var thisWeekStart = new Date(yesterday);
+thisWeekStart.setDate(thisWeekStart.getDate() - dowOffset);
+var thisWeekEnd = new Date(thisWeekStart);
+var friday = new Date(thisWeekStart);
+var saturday = new Date(thisWeekStart);
+var sunday  = new Date(thisWeekStart);
+friday.setDate(thisWeekEnd.getDate() + 4);
+saturday.setDate(thisWeekEnd.getDate() + 5);
+sunday.setDate(thisWeekEnd.getDate() + 6);
 
-      function matchWeekend(fruit) {
-          return fruit.selectedDate == 'Sun Apr 10 2016 00:00:00 GMT+0100 (BST)';
+// THESE DATES ARE CORRECT
+console.log(friday);
+console.log(saturday);
+console.log(sunday);
+
+//FIND ALL ENTRIES THAT FALL ON A WEEKEND
+function weekendPlans(callback) {
+  Entry.aggregate(
+      [
+          { "$redact": {
+              "$cond": {
+                  "if": {
+                      "$or": [
+                          { "$eq": [ { "$dayOfWeek": "$selectedDate" }, 1 ] },
+                          { "$eq": [ { "$dayOfWeek": "$selectedDate" }, 6 ] },
+                          { "$eq": [ { "$dayOfWeek": "$selectedDate" }, 7 ] }
+                      ]
+                  },
+                  "then": "$$KEEP",
+                  "else": "$$PRUNE"
+              }
+          }}
+      ],
+      // GET THE RESULTS AND RETURN IF selectedDate MATCHES THIS WEEKEND
+      function(err,results) {
+        var i = results.length;
+        var theWeekend;
+
+        while(i--) {
+          if(results[i].selectedDate === friday || saturday || sunday) {
+              theWeekend = results[i];
+              break;
+          }
+        }
+        callback(err, theWeekend)
       }
+)};
 
-      console.log(results.find(matchWeekend))
-    }
-)
- // { name: 'cherries', quantity: 5 }
 
 module.exports = router;
